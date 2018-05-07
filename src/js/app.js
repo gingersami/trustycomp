5be59cdf6c44ad82c5e6df0ac098ab0288ab5c04
Compie <earstuff@gmail.com>
2018-05-07 17:57:21 +0300

    s

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,13 @@
 import facebook from '../images/sprites/icons/sp-facebook.png';
 import twitter from '../images/sprites/icons/sp-twitter.png';
 import google from '../images/sprites/icons/sp-google.png';
-$('.answer').on('click', function () {
-    $(this).addClass('selected');
-    $(this).data('checked');
-    $(this).siblings().data('');
-    $(this).siblings().removeClass('selected');
-});
+
+// $('.answer').on('click', function () {
+//     $(this).addClass('selected');
+//     $(this).data('checked');
+//     $(this).siblings().data('');
+//     $(this).siblings().removeClass('selected');
+// });
 console.log('JS Running');
 
 
@@ -18,3 +19,15 @@ $('.search-results-filter-left p').hover(function(){
     $(this).siblings('.area-filter').hide()
 });
 */
+
+customElements.define('date-picker',
+    class extends HTMLElement {
+        constructor() {
+            super();
+            let template = document.getElementById('date-picker');
+            let templateContent = template.content;
+
+            const shadowRoot = this.attachShadow({mode: 'open'})
+                .appendChild(templateContent.cloneNode(true));
+        }
+    });
