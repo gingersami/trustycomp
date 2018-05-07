5040b1b1fe90be009285727798e2a0c490bf54f9
Compie <earstuff@gmail.com>
2018-05-07 09:59:24 +0300

    properly imports social logos

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,5 @@
+import facebook from '../images/sprites/icons/sp-facebook.png';
+import twitter from '../images/sprites/icons/sp-twitter.png';
+import google from '../images/sprites/icons/sp-google.png';
+
 console.log('JS Running');
